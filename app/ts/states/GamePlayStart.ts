917808d6facef41face321a13cba4a585ea676a5
Xavier Thomas <xthom001@gmail.com>
2016-01-23 20:23:52 -0500

    Creating starting screen

---
+++ app/ts/states/GamePlayStart.ts
@@ -0,0 +1,9 @@
+/// <reference path="../../libs/jquery.d.ts"/>
+/// <reference path="../../../node_modules/phaser/typescript/phaser.d.ts"/>
+
+module GameStates {
+  export class GamePlayState extends Phaser.State{
+    game: Phaser.Game;
+
+  }
+}
--- app/ts/states/titleScreenState.ts
+++ app/ts/states/titleScreenState.ts
@@ -12,16 +12,20 @@ module GameStates {
     preload(){
 
     }
+
     create(){
       this.game.add.sprite(0,0,'sky');
       // Welcome Text to start the game
       this.game.add.text(16, 16, 'Welcome to ContraRemake', { fontSize: '32px', fill: '#000' });
       // start button for the game
-      this.game.add.button(30,30, '', function(){
-        alert('HELLO WORLD');
-      })
+      var decrease = 200;
+      var width = this.game.width/2 - decrease;
+      var height = this.game.height/2 - decrease ;
+      this.game.add.button(height,width, 'btn', this.startGame);
     }
 
-
+    private startGame(): void{
+      this.game.state.start('GamePlayStart');
+    }
   }
 }
