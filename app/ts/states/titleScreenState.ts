41d3ab509fc77fcb9d91e2c7b026cbab1b11f479
Xavier Thomas <xthom001@gmail.com>
2016-01-23 22:34:51 -0500

    added in infinite scroll but need person on screen to test it out

--- app/ts/states/titleScreenState.ts
+++ app/ts/states/titleScreenState.ts
@@ -14,17 +14,18 @@ module GameStates {
     }
 
     create(){
-      this.game.add.sprite(0,0,'sky');
+      var background = this.game.add.sprite(0,0,'background');
+      background.width = this.game.width;
       // Welcome Text to start the game
-      var textX = this.game.width/2 - 200;
-      var textY = this.game.height/2 - 300;
+      var textX = this.game.width/2 - 150;
+      var textY = this.game.height/2 - 150 ;
       this.game.add.text(textX, textY, 'Welcome to ContraRemake', { fontSize: '32px', fill: '#000' });
 
       // start button for the game
-      var bDecrease = 100;
-      var bWidth = this.game.width/2 - bDecrease;
-      var bHeight = this.game.height/2 - bDecrease ;
-      this.game.add.button(bHeight,bWidth, 'btn', this.startGame);
+      var bDecrease = 350;
+      var bWidth = window.innerWidth/2;
+      var bHeight = window.innerHeight/3 - 100;
+      this.game.add.button(bWidth,bHeight, 'btn', this.startGame);
     }
 
     private startGame(): void{
