83cdf9d73f79d03bedfeffc43fc0b40fef811134
Xavier Thomas <xthom001@gmail.com>
2016-01-24 02:04:06 -0500

    working on fixing the collisons physics

--- app/ts/gameObjects/player.ts
+++ app/ts/gameObjects/player.ts
@@ -3,134 +3,55 @@
 /// <reference path="./weapons.ts"/>
 
 module Player {
-  abstract class playerConfig{
-    public static gravity: number = 500;
-    public static worldBounds: boolean = true;
-    public static leftAxis: number = -250;
-    public static rightAxis: number =250;
-    public static yAxis: number = -100;
-  }
-
-  export enum PlayerState {
-    IDLE, WALKING
-  }
-
-  export class playerOne  {
-    private damaged: boolean;
-    public game: Phaser.Game;
-    private weapons: Weapons.weapons;
-    private lifes: number;
-    private player: any;
-    private playerState: PlayerState;
-    private cursors: any;
-    private keyboardKeys: any;
-    private Escape: Phaser.Key;
-    private weaponSelection: boolean;
-
+  export class playerFactory {
+    private game: Phaser.Game;
+    private player: Phaser.Sprite;
+    private cursors: Phaser.CursorKeys;
 
     constructor(game: Phaser.Game) {
-      // initalizes game
       this.game = game;
 
-      this.player = this.game.add.sprite(32, this.game.world.height -100, 'dude');
+      // Initializes the player obj
+      this.player = this.game.add.sprite(52, game.world.height -80 , 'dude');
 
-      // Enables physics properties on the object
+      // Adds the player object to the physics
       this.game.physics.arcade.enable(this.player);
 
-      // Player physics properties. Give the little guy a slight bounce.
+      //  Player physics properties. Give the little guy a slight bounce.
       this.player.body.bounce.y = 0.2;
+      this.player.body.gravity.y = 300;
+      this.player.body.collideWorldBounds = true;
 
-      /*
-        Local gravity applied to this obj, if non zero overrides world gravity
-        unless body.allowGravity is set to false
-      */
-      this.player.body.gravity = playerConfig.gravity;
 
-      /*
-        A Body can be set to collide against the World bounds automatically and
-        rebound back into the World if this is set to true. Otherwise it will leave the World.
-      */
-      this.player.body.collideWorldBounds = playerConfig.worldBounds;
+      this.player.animations.add('left', [0, 1, 2, 3], 10, true);
+      this.player.animations.add('right', [5, 6, 7, 8], 10, true);
 
-      // returns an object containing up/down/left/right
       this.cursors = this.game.input.keyboard.createCursorKeys();
-
-      // Creates hot keys for the game
-      this.keyboardKeys = this.game.input.keyboard.addKeys(
-        {
-          sWeapon: Phaser.Keyboard.X,
-          fire: Phaser.Keyboard.C
-        }
-      )
-      this.weaponSelection = true;
-
-      // this.player.anchor.set(0.5,1.0);
     }
 
-    updateMovement(): void {
+    movement(): void {
+      //  Reset the players velocity (movement)
       this.player.body.velocity.x = 0;
 
       if (this.cursors.left.isDown) {
-        this.moveLeft();
+        //  Move to the left
+        this.player.body.velocity.x = -150;
+        this.player.animations.play('left');
       }
       else if (this.cursors.right.isDown) {
-        console.log(playerConfig.yAxis);
-        this.moveRight();
+        //  Move to the right
+        this.player.body.velocity.x = 150;
+        this.player.animations.play('right');
       }
       else {
-        this.standStill();
+        //  Stand still
+        this.player.animations.stop();
+        this.player.frame = 4;
       }
-
+      //  Allow the player to jump if they are touching the ground.
       if (this.cursors.up.isDown && this.player.body.touching.down) {
-        console.log(1,playerConfig.yAxis);
-        this.player.body.velocity.Y = playerConfig.yAxis;
+        this.player.body.velocity.y = -350;
       }
-
-      // if (this.keyboardKeys.sWeapon.isDown) {
-      //   this.weaponSelection = !this.weaponSelection;
-      // }
-      // if (this.keyboardKeys.fire.isDown) {
-      //   this.shoot();
-      // }
-    }
-
-    private moveRight(): void {
-      this.player.body.velocity.x = playerConfig.rightAxis;
-      this.player.animations.play('right');
-    }
-
-    private standStill() {
-      this.player.animations.stop();
-      this.player.frame = 5;
-    }
-
-    private moveLeft(): void {
-      this.player.body.velocity.x = playerConfig.leftAxis;
-      this.player.animations.play('left');
-    }
-
-    private jump(config: playerConfig): void {
-      this.player.body.velocity.y = playerConfig.yAxis;
-    }
-
-    private shoot(): void {
-
-    }
-
-    setWeapons(weapon: any, weaponOption: number): void {
-      this.weapons.setCurrentWeapon(weapon, weaponOption);
-    }
-
-    getCurrentWeapon(): Weapons.weapons {
-      return this.weapons.getCurrentWeapon();
-    }
-
-    // getSecondWeapon(): Weapons.weapons {
-    //   return this.weapons.getSecondWeapon()
-    // }
-    //initiates player.
-    addAnimations(direction: string, frames: number[], speed: number, loop: boolean) {
-      this.player.animations.add(direction, frames, speed, loop);
     }
   }
 }
--- app/ts/states/GamePlayStart.ts
+++ app/ts/states/GamePlayStart.ts
@@ -5,10 +5,9 @@
 module GameStates {
   export class GamePlayStart extends Phaser.State{
     game: Phaser.Game;
-    player: Player.playerOne;
-
+    player: Player.playerFactory;
     private bgTile;
-    private plat
+    private platforms: Phaser.Group;
 
     constructor(){
       super();
@@ -18,36 +17,35 @@ module GameStates {
 
     }
     create(){
+
+      //  We're going to be using physics, so enable the Arcade Physics system
+      this.game.physics.startSystem(Phaser.Physics.ARCADE);
       this.bgTile = this.game.add.tileSprite(0, 0, this.game.width, this.game.height, 'background');
 
-      var platforms = this.game.add.group();
-
+      this.platforms = this.game.add.group();
       // allows all existing and new sprites to have physics body enabled
-      platforms.enableBody = true;
+      this.platforms.enableBody = true;
 
       //creates ground
-      var ground = platforms.create(0,this.game.height -34, 'platform');
+      var ground = this.platforms.create(0,this.game.height -34, 'platform');
       //  Scale it to fit the width of the game (the original sprite is 400x32 in size)
       ground.scale.setTo(4, 2);
       //  This stops it from falling away when you jump on it
       ground.body.immovable = true;
 
-      var ledge = platforms.create(this.game.width -200, 100, 'platform');
-      // ledge.scale.setTo(2,2);
+      var ledge = this.platforms.create(this.game.width -200, 100, 'platform');
       ledge.body.immovable = true;
 
-      ledge = platforms.create(400, 250, 'platform');
-      // ledge.scale.setTo(2,2);
+      ledge = this.platforms.create(400, 250, 'platform');
       ledge.body.immovable = true;
 
-      this.player = new Player.playerOne(this.game);
-      this.player.addAnimations('left', [0, 1, 2, 3], 10, true);
-      this.player.addAnimations('right', [5, 6, 7, 8], 10, true);
+
+      this.player = new Player.playerFactory(this.game);
     }
     update(){
+      this.game.physics.arcade.collide(this.player, this.platforms);
       this.bgTile.tilePosition.x -= 1;
-      this.player.updateMovement();
-
+      this.player.movement();
     }
 
   }
